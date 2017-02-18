6800717b6ecaca041c41f2474f4878c8369a1cdc
Joel Benjamin <joelbenjamin093@gmail.com>
2017-02-18 09:41:35 +0530

    Add files via upload

--- weather-app/app.py
+++ weather-app/app.py
@@ -3,22 +3,47 @@ from flask import render_template
 from Extractor import Wex
 
 
+
+def ChangeImg(data):
+    '''
+    returns the weather icon based on the weather
+    '''
+    Weather_conditions = ['Clear','Rain','Sunny','Clouds']
+    for x in Weather_conditions:
+        if x == data[0]:
+            return x
+    return 'Clouds'
+
+def BuildImgPath(image_name):
+    '''
+    Builds the path for the image icon in the static folder
+    '''
+    image_path = "/static/{0}.png".format(image_name)
+    return image_path
+
 def GetWeatherData(searchquery):
+    '''
+    Sends a request to the OpenWeather server and it returns a weather object.
+    Uses an external tool to extract place mentions and request the weather of that place.
+    '''
     weather = Wex.WexWeather(searchquery,"396cbf110cc7bd1bd3087f317643a83f")
-    data = weather.WexGetWeather()
-    city = weather.GetCityNames()
-    data += city
-    print(data)
+    # adds the data to the data list
+    data = weather.WexGetWeather() + weather.GetCityNames()
+    # Changes the weather icon based on the current weather
+    ImageName = ChangeImg(data)
+    # Builds the path to the image icon
+    imagePath = BuildImgPath(ImageName)
+    data.append(imagePath)
     return data
 
 app = Flask(__name__)
 
 @app.route('/')
 def startup():
-    weatherInfo = ['None']*5
+    weatherInfo = []
     return render_template('startpage.html',data = weatherInfo)
 
-@app.route('/',methods=['POST','GET'])
+@app.route('/',methods=['POST'])
 def startpage():
     error_msg = None
     #weatherInfo = ['21','rain']
